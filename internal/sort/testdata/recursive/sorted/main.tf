3de78619a5a4f78ddf2eb85ef81cffb23017e986
Dan Thagard <dthagard@gmail.com>
2026-02-27 10:29:58 -0500

    feat(sort): add --recursive flag for nested directory processing
    Walk the target directory tree and independently sort each sub-directory
    that contains .tf files. Supports --inline, --output-dir (mirroring
    directory structure), --check, and --exclude in recursive mode.

---
+++ internal/sort/testdata/recursive/sorted/main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "app" {
+  ami           = "ami-67890"
+  instance_type = "t2.small"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-12345"
+  instance_type = "t2.micro"
+}
